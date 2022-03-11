ce762845e1c90378e16a607412c3ed6ec399b063
Arthur Schopenhauer <lokigoldx8@gmail.com>
2022-03-11 10:33:57 +0300

    Если пуста корзина, то вставляю иконку пустоты

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { CustomContext } from '../../Context';
+import images from '../../assets/cart.png';
 
 const Cart = () => {
 
@@ -7,38 +8,49 @@ const Cart = () => {
 
 	return (
 		<div className="container">
-			<table className="centered">
-				<thead>
-					<tr>
-						<th>Shoes Name</th>
-						<th>Shoes Count</th>
-						<th>Action</th>
-						<th>Shoes Price</th>
-					</tr>
-				</thead>
-				<tbody>
-					{cart.map((item, idx) => {
-						return (
-							<tr key={item.shoe[0].id}>
-								<td>{item.shoe[0].shoe}</td>
-								<td>{item.count}</td>
-								<td className="cart__buttons-row">
-									<button type="button" className="cart__button-plus" onClick={() => plusCountCart(item.shoe[0].id)}>+</button>
-									<button type="button" className="cart__button-minus" onClick={() => minusCountCart(item.shoe[0].id)}>-</button>
-								</td>
-								<td>$ {item.shoe[0].retailPrice * item.count}</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
 
-			<div className="cart__foot">
-				<p className="cart__total">Total price: ${cart.reduce((acc, rec) => {
-					return acc + rec.count * rec.shoe[0].retailPrice;
-				}, 0)} </p>
-				<button className="cart__pay btn" type="button">Pay</button>
-			</div>
+			{cart.length === 0
+				? <>
+					<h2 className="cart__title">Cart is empty</h2>
+					<img className="cart__img" src={images} alt="Cart is empty" />
+				</>
+				: <>
+					<table className="centered">
+						<thead>
+							<tr>
+								<th>Shoes Name</th>
+								<th>Shoes Count</th>
+								<th>Action</th>
+								<th>Shoes Price</th>
+							</tr>
+						</thead>
+						<tbody>
+							{cart.map((item, idx) => {
+								return (
+									<tr key={item.shoe[0].id}>
+										<td>{item.shoe[0].shoe}</td>
+										<td>{item.count}</td>
+										<td className="cart__buttons-row">
+											<button type="button" className="cart__button-plus" onClick={() => plusCountCart(item.shoe[0].id)}>+</button>
+											<button type="button" className="cart__button-minus" onClick={() => minusCountCart(item.shoe[0].id)}>-</button>
+										</td>
+										<td>$ {item.shoe[0].retailPrice * item.count}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+
+					<div className="cart__foot">
+						<p className="cart__total">Total price: ${cart.reduce((acc, rec) => {
+							return acc + rec.count * rec.shoe[0].retailPrice;
+						}, 0)} </p>
+						<button className="cart__pay btn" type="button">Pay</button>
+					</div>
+				</>
+			}
+
+
 
 		</div>
 	);
